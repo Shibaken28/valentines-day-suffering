9ba4f1a57675b4f708be9796ecdb1835101cf002
Shibaken28 <8128kanzensu@gmail.com>
2022-02-14 20:11:50 +0900

    バージョン表示

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@
 let red = [0x61,70,240,247,0x91,0x31];
 let green = [0x2b,150,240,173,0x4b,0x1b];
 let blue = [0x20,70,180,195,0x40,0x10];
+let version="ver. 0.8.2";
 
       /*座標*/
 class pos{
@@ -292,6 +293,9 @@ function draw() {
   background("#EEEEEE");
   drawBackground();
   rotate(2*PI-0.1);
+  fill(0);
+  textAlign(LEFT, TOP);
+  text(version,10,10);
   blendMode(BLEND);
   board.draw();
   if(mouseIsPressed){
